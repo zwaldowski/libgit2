b816294abd0a31189215af19cb726773e40c838e
Pierre-Olivier Latour <pol@mac.com>
2015-03-03 20:14:12 -0800

    Exposed new SPI git_branch_upstream_name_from_merge_remote_names()

--- src/branch.c
+++ src/branch.c
@@ -331,6 +331,45 @@ static int retrieve_upstream_configuration(
 	return error;
 }
 
+int git_branch_upstream_name_from_merge_remote_names(
+	git_buf *out,
+	git_repository *repo,
+	const char *remote_name,
+	const char *merge_name)
+{
+	git_buf buf = GIT_BUF_INIT;
+	int error = -1;
+	git_remote *remote = NULL;
+	const git_refspec *refspec;
+
+	assert(out && remote_name && merge_name);
+
+	git_buf_sanitize(out);
+
+	if (strcmp(".", remote_name) != 0) {
+		if ((error = git_remote_lookup(&remote, repo, remote_name)) < 0)
+			goto cleanup;
+
+		refspec = git_remote__matching_refspec(remote, merge_name);
+		if (!refspec) {
+			error = GIT_ENOTFOUND;
+			goto cleanup;
+		}
+
+		if (git_refspec_transform(&buf, refspec, merge_name) < 0)
+			goto cleanup;
+	} else
+		if (git_buf_set(&buf, merge_name, strlen(merge_name)) < 0)
+			goto cleanup;
+
+	error = git_buf_set(out, git_buf_cstr(&buf), git_buf_len(&buf));
+
+cleanup:
+	git_remote_free(remote);
+	git_buf_free(&buf);
+	return error;
+}
+
 int git_branch_upstream_name(
 	git_buf *out,
 	git_repository *repo,
@@ -338,15 +377,10 @@ int git_branch_upstream_name(
 {
 	git_buf remote_name = GIT_BUF_INIT;
 	git_buf merge_name = GIT_BUF_INIT;
-	git_buf buf = GIT_BUF_INIT;
 	int error = -1;
-	git_remote *remote = NULL;
-	const git_refspec *refspec;
 	git_config *config;
 
 	assert(out && refname);
-
-	git_buf_sanitize(out);
 
 	if (!git_reference__is_branch(refname))
 		return not_a_local_branch(refname);
@@ -369,30 +403,12 @@ int git_branch_upstream_name(
 		goto cleanup;
 	}
 
-	if (strcmp(".", git_buf_cstr(&remote_name)) != 0) {
-		if ((error = git_remote_lookup(&remote, repo, git_buf_cstr(&remote_name))) < 0)
-			goto cleanup;
-
-		refspec = git_remote__matching_refspec(remote, git_buf_cstr(&merge_name));
-		if (!refspec) {
-			error = GIT_ENOTFOUND;
-			goto cleanup;
-		}
-
-		if (git_refspec_transform(&buf, refspec, git_buf_cstr(&merge_name)) < 0)
-			goto cleanup;
-	} else
-		if (git_buf_set(&buf, git_buf_cstr(&merge_name), git_buf_len(&merge_name)) < 0)
-			goto cleanup;
-
-	error = git_buf_set(out, git_buf_cstr(&buf), git_buf_len(&buf));
+	error = git_branch_upstream_name_from_merge_remote_names(out, repo, git_buf_cstr(&remote_name), git_buf_cstr(&merge_name));
 
 cleanup:
 	git_config_free(config);
-	git_remote_free(remote);
 	git_buf_free(&remote_name);
 	git_buf_free(&merge_name);
-	git_buf_free(&buf);
 	return error;
 }
 
