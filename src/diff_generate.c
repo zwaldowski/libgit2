a83a274929d147af09c3833bcb21f892ae90118a
Pierre-Olivier Latour <pol@mac.com>
2016-12-27 18:22:34 +0900

    Add time metadata to git_diff_file

--- src/diff_generate.c
+++ src/diff_generate.c
@@ -169,12 +169,16 @@ static int diff_delta__from_one(
 		delta->old_file.flags |= GIT_DIFF_FLAG_EXISTS;
 		git_oid_cpy(&delta->old_file.id, &entry->id);
 		delta->old_file.id_abbrev = GIT_OID_HEXSZ;
+		delta->old_file.mtime = entry->mtime.seconds;
+		delta->old_file.ctime = entry->ctime.seconds;
 	} else /* ADDED, IGNORED, UNTRACKED */ {
 		delta->new_file.mode = entry->mode;
 		delta->new_file.size = entry->file_size;
 		delta->new_file.flags |= GIT_DIFF_FLAG_EXISTS;
 		git_oid_cpy(&delta->new_file.id, &entry->id);
 		delta->new_file.id_abbrev = GIT_OID_HEXSZ;
+		delta->new_file.mtime = entry->mtime.seconds;
+		delta->new_file.ctime = entry->ctime.seconds;
 	}
 
 	delta->old_file.flags |= GIT_DIFF_FLAG_VALID_ID;
@@ -230,6 +234,8 @@ static int diff_delta__from_two(
 		delta->old_file.id_abbrev = GIT_OID_HEXSZ;
 		delta->old_file.flags |= GIT_DIFF_FLAG_VALID_ID |
 			GIT_DIFF_FLAG_EXISTS;
+		delta->old_file.mtime = old_entry->mtime.seconds;
+		delta->old_file.ctime = old_entry->ctime.seconds;
 	}
 
 	if (!git_index_entry_is_conflict(new_entry)) {
@@ -239,6 +245,8 @@ static int diff_delta__from_two(
 		delta->new_file.mode = new_mode;
 		delta->old_file.flags |= GIT_DIFF_FLAG_EXISTS;
 		delta->new_file.flags |= GIT_DIFF_FLAG_EXISTS;
+		delta->new_file.mtime = new_entry->mtime.seconds;
+		delta->new_file.ctime = new_entry->ctime.seconds;
 
 		if (!git_oid_iszero(&new_entry->id))
 			delta->new_file.flags |= GIT_DIFF_FLAG_VALID_ID;
